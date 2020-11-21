da218c4f3a94724159cf6d42154b37de0396f01b
Min-Haw, Liu <liuminhaw@gmail.com>
2020-11-21 11:34:11 +0800

    update: 02_jsx-conditio and 03_app-structure

---
+++ 02_jsx-condition/index.js
@@ -0,0 +1,21 @@
+function getNum() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+class NumPicker extends React.Component {
+    render() {
+        const num = getNum();
+        return (
+            <div>
+                <h1>Your number is {num}</h1>
+                <p>{num === 7 ? 'Congrats!' : 'Unlucky!'}</p>
+                {
+                    num === 7 &&
+                        <img src="https://media.giphy.com/media/MDswnD2fjSB2kUzMxI/giphy.gif"/>
+                }
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<NumPicker />, document.getElementById('root'));
